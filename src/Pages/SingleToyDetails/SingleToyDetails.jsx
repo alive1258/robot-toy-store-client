507f53001dd54eb45db10448fce67844e0e218a4
alive1258 <javashazib@gmail.com>
2023-05-21 21:58:33 +0600

    aos package use in home page done

--- src/Pages/SingleToyDetails/SingleToyDetails.jsx
+++ src/Pages/SingleToyDetails/SingleToyDetails.jsx
@@ -4,31 +4,42 @@ import { useLoaderData } from 'react-router-dom';
 const SingleToyDetails = () => {
     // const toyDetails=useLoaderData()
     const toyDetails = useLoaderData()
-    const {_id, toyName, price, quantity, photo, rating, description } = toyDetails
+    const {_id, toyName, price, quantity, pictureUrl, rating, description } = toyDetails
  
     console.log(toyDetails)
 
     return (
-        <div>
-            SingleToyDetails:{toyName}
-
-
-<div>
-<div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src={photo} alt="Album"/></figure>
+        <div className='mt-20 px-20'>
+     <div className="card lg:card-side bg-base-100 shadow-2xl">
+  <figure><img src={pictureUrl} alt="Album"/></figure>
   <div className="card-body">
     <h2 className="card-title">{toyName}</h2>
-    <p>{price}</p>
-    <p>{quantity}</p>
-    <p>{rating}</p>
-    <p>{description}</p>
+    <p>${price}</p>
+    <p>Quantity: {quantity}</p>
+    <p>Rating: {rating}</p>
+    <p>Description: {description}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
+   
     </div>
   </div>
 </div>
+
+
+
+{/* <div className='px-20'>
+<div className="card bg-base-100 shadow-2xl">
+  <img src={pictureUrl} alt="" />
+  <div className="card-body">
+    <h2 className="card-title">ToyName: {toyName}</h2>
+    <p>${price}</p>
+    <p>Quantity: {quantity}</p>
+    <p>Rating: {rating}</p>
+    <p>Description: {description}</p>
+  
+  </div>
 </div>
-            
+</div>
+             */}
             
         </div>
     );
